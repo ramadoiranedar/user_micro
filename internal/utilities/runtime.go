36440cd24e07679acda2be7a565458c28a037391
Raden Ario Damar <raden.ario@lintasarta.co.id>
2023-11-25 19:10:11 +0700

    [SKELETON adjustment utilities runtime

--- internal/utilities/runtime.go
+++ internal/utilities/runtime.go
@@ -1,89 +1,68 @@
 package utilities
 
 import (
-	"net/http"
 	"os"
 
-	"github.com/go-openapi/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"gorm.io/gorm"
 )
 
-/**
-*	@Cfg as viper config
-* @Log as logrus logger
-* @Db as gorm DB
- */
-type Runtime struct {
-	Cfg *viper.Viper
-	Log logrus.FieldLogger
-	Db  *gorm.DB
+type runtimeApp struct {
+	config   *viper.Viper
+	logger   logrus.FieldLogger
+	database *gorm.DB
 }
 
-func InitRuntime(configuration *viper.Viper, logger logrus.FieldLogger) (runtime *Runtime) {
+func NewRuntime(configuration *viper.Viper, logger logrus.FieldLogger) (r *runtimeApp) {
 	var (
-		logFields = CreateLogFields(logger, TraceLog())
+		logEntry = EntryLogTrace(logger, TraceLog())
 	)
 
-	logFields.Info("setup runtime")
+	logEntry.Info("setup runtime")
 
-	database, err := SetGormDbMysql(configuration, logger)
+	database, err := GetGormDbMysql(configuration, logger)
 	if err != nil {
-		logFields.Errorf("database connection error: %v", err)
+		logEntry.Errorf("database connection error: %v", err)
 		os.Exit(1)
 	}
 
-	runtime = &Runtime{
-		Cfg: configuration,
-		Log: logger,
-		Db:  database,
+	r = &runtimeApp{
+		config:   configuration,
+		logger:   logger,
+		database: database,
 	}
+
+	// run migration every start the server
+	// r.MigrateDatabase()
 
 	return
 }
 
-func (rt *Runtime) Database() *gorm.DB {
-	return rt.Db
+func (r *runtimeApp) Database() *gorm.DB {
+	return r.database
 }
 
-func (rt *Runtime) Logger() logrus.FieldLogger {
-	return rt.Log
+func (r *runtimeApp) Logger() logrus.FieldLogger {
+	return r.logger
 }
 
-func (rt *Runtime) Config() *viper.Viper {
-	return rt.Cfg
+func (r *runtimeApp) Config() *viper.Viper {
+	return r.config
 }
 
-func (rt *Runtime) RunMigration() {
-	CreateLogFields(rt.Log, TraceLog()).
+func (r *runtimeApp) Close() {
+	EntryLogTrace(r.logger, TraceLog()).
+		Infof("%v", "runtime close")
+	// TODO: Close at Runtime
+}
+
+func (r *runtimeApp) migrateDatabase() {
+	EntryLogTrace(r.logger, TraceLog()).
 		Info("run migration database")
 
-	rt.Db.AutoMigrate(
+	r.database.AutoMigrate(
 	// TODO: Migration
 	// models.User{},
 	)
-}
-
-func (rt *Runtime) SetError(code int, msg string, args ...interface{}) error {
-	CreateLogFields(rt.Log, TraceLog()).
-		Infof("%v", msg)
-
-	return errors.New(int32(code), msg)
-}
-
-func (rt *Runtime) GetError(err error) errors.Error {
-	CreateLogFields(rt.Log, TraceLog()).
-		Errorf("%v", err)
-	if v, ok := err.(errors.Error); ok {
-		return v
-	}
-
-	return errors.New(http.StatusInternalServerError, err.Error())
-}
-
-func (rt *Runtime) Close() {
-	CreateLogFields(rt.Log, TraceLog()).
-		Infof("%v", "runtime close")
-	// TODO: Close
 }
