3730771a114c0cc461647635abad7e4fa6c2611d
Raden Ario Damar <raden.ario@lintasarta.co.id>
2023-11-25 19:09:54 +0700

    [SKELETON] adjustment utilities database

--- internal/utilities/database.go
+++ internal/utilities/database.go
@@ -3,29 +3,32 @@ package utilities
 import (
 	"fmt"
 
+	"github.com/ramadoiranedar/user_micro/internal/constants"
+	str "github.com/ramadoiranedar/user_micro/internal/constants/strings"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	gorm_logger "gorm.io/gorm/logger"
 )
 
-func SetGormDbMysql(config *viper.Viper, log logrus.FieldLogger) (*gorm.DB, error) {
-	CreateLogFields(logrus.StandardLogger(), TraceLog()).Info("setup gorm database mysql")
+func GetGormDbMysql(config *viper.Viper, logrus logrus.FieldLogger) (*gorm.DB, error) {
+	EntryLogTrace(logrus, TraceLog()).Info("setup gorm database mysql")
 
 	dsn := GetGormDsnMysql(config)
 	return gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: gorm_logger.Default.LogMode(gorm_logger.Info),
 	})
 }
 
 func GetGormDsnMysql(config *viper.Viper) string {
+	c := constants.NewConstants(config)
 	return fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		config.GetString("db.user"),
-		config.GetString("db.pass"),
-		config.GetString("db.host"),
-		config.GetString("db.port"),
-		config.GetString("db.name"),
+		str.DSN_MYSQL_FORMAT,
+		c.GetDbUser(),
+		c.GetDbPass(),
+		c.GetDbHost(),
+		c.GetDbPort(),
+		c.GetDbName(),
 	)
 }
